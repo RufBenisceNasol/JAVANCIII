aee7d53c1266018daad1bac5d44b51674ae288a2
RufBenisceNasol <nasolruf@gmail.com>
2024-10-08 08:14:51 +0000

    submitTasks

---
+++ Tasksheet113.java
@@ -0,0 +1,15 @@
+public class Tasksheet113 {
+    public static void main(String[] args) {
+        byte zero = 0;
+        char H = 'H';
+        char r= 'r';
+        int three = 3;
+        int one = 1;
+        float twoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        String output = "" + H + three + one + one + zero + " w" + zero + r + one + "d " + twoPointZero + " " + isTrue;
+        System.out.println(output);
+    }
+}
+    
---
+++ Tasksheet114.java
@@ -0,0 +1,10 @@
+public class Tasksheet114 {
+    public static void main(String[] args) {
+
+    for(int i =1 ; i <= 10; i++){
+        String message = (i%2==0) ? i+" is even number" : i+" is odd number";
+        System.out.println(message);
+    }
+    
+}
+}
