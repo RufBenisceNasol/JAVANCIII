cfdf0a58aa9769eae8028a6fe276fe561c30012f
RufBenisceNasol <nasolruf@gmail.com>
2024-10-31 18:27:16 +0800

    Submit Task

---
+++ Tasksheet126.java
@@ -0,0 +1,40 @@
+class Vehicle{
+    String make;
+    String model;
+    int year;
+
+    public Vehicle(String make, String model, int year){
+        this.make=make;
+        this.model= model;
+        this.year= year;
+    }
+}
+
+class Car extends Vehicle{
+    
+    int numOfDoors;
+
+    public Car(String make, String model, int year, int numOfDoors){
+        super(make, model, year);
+        this.numOfDoors=numOfDoors;
+    }
+
+    public void carDetails(){
+        System.out.println("Car Details:");
+        System.out.println("Make: "+ make);
+        System.out.println("Model: "+ model);
+        System.out.println("Year: "+ year);
+        System.out.println("Number of Doors: "+ numOfDoors);
+
+    }
+
+}
+
+
+public class Tasksheet126 {
+    public static void main(String[] args) {
+        Car car1= new Car("Honda", "Civic", 2015, 4);
+
+        car1.carDetails();
+    }
+}
